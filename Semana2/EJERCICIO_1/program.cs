856be67fa425549b9992cf9aaa945c7da0d4042a
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-04-21 01:41:14 -0500

    Add files via upload

---
+++ Semana2/EJERCICIO_1/program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace cuadro
+
+{
+    class cuadro
+    {
+        static void Main(string[] args)
+        {
+            string[] dim = new string[5];
+
+            dim[4] = "111111111111111";
+
+            dim[1] = "100000000000001";
+
+            dim[3] = "100000000000001";
+
+            dim[2] = "100000000000001";
+
+            dim[0] = "111111111111111";
+
+
+            Console.WriteLine("Cuadro");
+
+            Console.WriteLine("{dim[0]}");
+
+            Console.WriteLine("{dim[1]}");
+
+            Console.WriteLine("{dim[2]}");
+
+            Console.WriteLine("{dim[3]}");
+
+            Console.WriteLine("{dim[4]}");
+
+        }
+    }
+}
+
