7b52c7c054f6258ec2f08f830dc72bd3bde9d19a
NicolassCa <99280034+NicolassCa@users.noreply.github.com>
2022-04-21 01:37:24 -0500

    Add files via upload

---
+++ Semana1/Ej11/Ejercicio 11.cs
@@ -0,0 +1,33 @@
+using System;
+namespace ejercicio_11
+    
+{  
+ class matrizEjercicio
+   {
+         string paises = new string[3];
+        string ciudades = new string[3];
+        public void CargarMatriz()
+        {
+            for (int f = 0; f < paises.Length; f++)
+            {
+                Console.Write($"Hola, Bienvenido, Digite el nombre de su pais {f + 1}: ");
+                paises[f] = Console.ReadLine();
+
+                
+                for (int c = 0; c < ciudades.GetLength(1); c++)
+                {
+                    Console.Write($"Ingrese el numero de ciudades {c + 1}: ");
+                    ciudades[f] =(System.Console.ReadLine());
+                }
+            }
+        }
+
+      static void Main(string[] args)
+      {
+        matrizEjercicio matrizejercicio = new matrizEjercicio();
+            matrizEjercicio.CargarMatriz();
+        
+      }
+    }
+}
+
